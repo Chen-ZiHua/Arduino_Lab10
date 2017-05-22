d3e3a6fb92fc36f36dc9d4b96d42b50f49e7536d
陳姿華 <isa19970825@gmail.com>
2017-05-22 19:21:33 +0800

    Lab10_2發射紅外線編碼 並可由另一台正確接收

---
+++ lab10_2.ino
@@ -0,0 +1,19 @@
+#include<IRremote.h>
+const int buttonPin=4;
+int buttonState=0;
+
+IRsend irsend;
+void setup()
+{
+  pinMode(buttonPin,INPUT);
+}
+
+void loop()
+{
+  buttonState = digitalRead(buttonPin);
+  if(buttonState==HIGH)
+  {
+    irsend.sendNEC(0x04FB48B7,32);
+  }
+}
+
